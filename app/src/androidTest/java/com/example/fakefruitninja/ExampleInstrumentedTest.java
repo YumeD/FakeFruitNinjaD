dcd25a42189cb8edd60eea87d01edf1864851769
sakuragts <BoboboPuniPotato44>
2019-12-24 19:59:39 -0500

    first commit again213

--- app/src/androidTest/java/com/example/fakefruitninja/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/fakefruitninja/ExampleInstrumentedTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.*;
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * test commit12
+ * test commit122
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
